121234d561f0dd8e90c537c5865a1044711f3d9f
VASS\adrian.sanzl <adrian.sanzl@vass.es>
2022-11-23 12:17:08 +0100

    nuevas utilidades onclick y adaptador

--- app/src/main/java/com/example/chistes/OnclickListener.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chistes
-
-interface OnclickListener {
-
-    fun onClick(chiste: Chiste)
-}
---
+++ app/src/main/java/com/example/chistes/ui/utils/OnclickListener.kt
@@ -0,0 +1,8 @@
+package com.example.chistes.ui.utils
+
+import com.example.chistes.ui.views.models.Chiste
+
+interface OnclickListener {
+
+    fun onClick(chiste: Chiste)
+}
